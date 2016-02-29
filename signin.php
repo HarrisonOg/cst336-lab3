2d0d8be8bbbb46e0c44bfc0f643f5c4b3a08255a
Harrison Oglesby <oglesby.harrison@gmail.com>
2016-02-29 03:20:47 +0000

    added background image and beginning form

---
+++ signin.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>Sign in</title>
+        <link rel="stylesheet" type="css" href="css/main.css">
+    </head>
+    <body background="assets/clouds.jpg">
+        <div align='center'>
+            <h1>Enter in Player names</h1>
+            <form>
+                <div>
+                    <label>Player 1: </label><input type="text" name = "p1" />
+                </div>
+                <br/>
+                <div>
+                    <label>Player 2: </label><input type="text" name = "p2" />
+                </div>
+                <br/>
+                <div>
+                    <label>Player 3: </label><input type="text" name = "p3" />
+                </div>
+                <br/>
+                <div>
+                    <label>Player 4: </label><input type="text" name = "p4" />
+                </div>
+                <br/>
+                
+                <input class="formButtons" type="submit" value="Submit"/>
+                <input class="formButtons" type="reset" value="Reset"/>
+            </form>
+        </div>        
+    </body>
+</html>
